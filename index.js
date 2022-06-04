40e3dd35f25e4d84c10108ff3571394b0a213595
brian ambundo <brian.katiechi9@gmail.com>
2022-06-04 22:49:34 +0300

    commit tree 30d5b52da4511029c48afbb87378a2780bd2bd4d

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code here!
+const topMain = document.getElementById('main');
+topMain.remove();
+
+
+const newHeader  = document.createElement("h1");
+newHeader .setAttribute('id','victory')
+newHeader .textContent = `YOUR-NAME is the champion`
+document.body.appendChild(newHeader )
+
+
+
